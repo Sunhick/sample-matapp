1e60fe9d1aa9066aa508d6e6c3849b0f69ff23d0
Sunil <sunhick@gmail.com>
2016-09-01 15:05:12 -0600

    added dependency to submodule

--- matapp/main.c
+++ matapp/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include "../modules/matlib/matlib/inc/matlib.h"
 
 void main(int argc, char** argv) {
 	printf("Welcome to git submodules example\n ");
+	printf("square=%d\n", Square(87));
 }
